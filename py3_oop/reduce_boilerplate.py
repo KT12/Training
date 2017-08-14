1fc33b899069274e9c6f8cc2cf370c7af8331b33
Ken Tsuji <nyy2008@gmail.com>
2017-08-14 19:13:03 -0400

    Tests for stats

---
+++ py3_oop/reduce_boilerplate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 19:00:52 2017
+
+@author: kt12
+"""
+
+from stats import StatsList
+import unittest
+
+class TestValidInputs(unittest.TestCase):
+    def setUp(self):
+        self.stats = StatsList([1,2,2,3,3,4])
+    
+    def test_mean(self):
+        self.assertEqual(self.stats.mean(), 2.5)
+        print(self.stats)
+    
+    def test_median(self):
+        self.assertEqual(self.stats.median(), 2.5)
+        self.stats.append(4)
+        self.assertEqual(self.stats.mode(), 3)
+        print(self.stats)
+
+    def test_mode(self):
+        self.assertEqual(self.stats.mode(), [2,3])
+        self.stats.remove(2)
+        self.assertEqual(self.stats.mode(), [3])
+        print(self.stats)
+
+if __name__ == '__main__':
+    unittest.main()
